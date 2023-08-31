95773ea9049c32474dec23da6aa12edb89189016
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-08-31 23:14:58 +0900

    ✨ 最速で学びたいTypeScript入門 #2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,46 @@ const increment = (x: number, y: number): number => {
   return x + y;
 };
 
+// Intersection Types
+
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: 'Tokyo',
+  username: 'xxx',
+  password: 'yyy',
+};
+
+// Union Types
+
+let value: boolean | number;
+value = true;
+// value = '10';
+value = 10;
+
+let arrayUni: (number | string)[];
+arrayUni = [0, 1, 2, 'hello'];
+
+// Literal Types
+
+let company: 'Facebook' | 'Google' | 'Amazon';
+company = 'Amazon';
+// company = 'Apple';
+let memory: 256 | 512;
+memory = 256;
+// memory = 128;
+
 function App() {
   return (
     <>
