9133736110ef3fb2ef9237dcf73824d5cf2d0cf0
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-09-06 23:03:35 +0900

    📝 最速で学びたいTypeScript入門 #2

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,73 @@ let memory: 256 | 512;
 memory = 256;
 // memory = 128;
 
+// typeof
+
+let msg: string = 'Hi';
+let msg2: typeof msg;
+msg2 = 'hello';
+
+let animal = { cat: 'small cat' };
+let newAnimal: typeof animal = { cat: 'big cat' };
+
+// keyof
+
+type KEYS = {
+  primary: string;
+  secondary: string;
+};
+
+let key: keyof KEYS;
+key = 'primary';
+
+// typeof + keyof
+
+const SPORTS = {
+  soccer: 'Soccer',
+  baseball: 'Baseball',
+};
+
+let keySports: keyof typeof SPORTS;
+keySports = 'soccer';
+
+// enum
+
+enum OS {
+  Windows,
+  Mac,
+  Linux,
+}
+
+interface PC {
+  id: number;
+  OSType: OS;
+}
+
+const PC1: PC = {
+  id: 1,
+  OSType: OS.Windows,
+};
+
+const PC2: PC = {
+  id: 2,
+  OSType: OS.Mac,
+};
+
+// 型の互換性
+
+const comp1 = 'test';
+let comp2: string = comp1;
+
+let comp3: string = 'test';
+// let comp4: 'test' = comp3;
+
+let funcComp1 = (x: number) => {};
+
+let funcComp2 = (x: string) => {};
+
+// funcComp1 = funcComp2;
+// funcComp2 = funcComp1;
+
 function App() {
   return (
     <>
